f1fe8ce1ab3d41cebb529f59e6d6b286279a075e
creaseaj <creaseaj@gmail.com>
2020-12-08 03:30:44 +0000

    Day 1 Added

---
+++ AdventOfCode2020/adventOfCode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace AdventOfCode2020
+{
+    static class adventOfCode
+    {
+        static public class day1
+        {
+            static public int findNumbers(string numberList)
+            {
+                int numberOut = 0;
+                Regex regexObj = new Regex(@"\n");
+                Match matchResults = regexObj.Match(numberList);
+                string[] theList = Regex.Split(numberList, @"\n");
+                for (int i = 0; i < theList.Length; i++)
+                {
+                    for (int j = 0; j < theList.Length; j++)
+                    {
+                        if (Convert.ToInt32(theList[i]) + Convert.ToInt32(theList[j]) == 2020){
+                            numberOut = Convert.ToInt32(theList[i]) * Convert.ToInt32(theList[j]);
+                            break;
+                        }
+                    }
+                }
+                return numberOut;
+            }
+            
+            static public int find3Numbers(string NumberList)
+            {
+                int numberOut = 0;
+                Regex regexObj = new Regex(@"\n");
+                Match matchResults = regexObj.Match(NumberList);
+                string[] theList = Regex.Split(NumberList, @"\n");
+                for (int i = 0; i < theList.Length; i++)
+                {
+                    for (int j = 0; j < theList.Length; j++)
+                    {
+                        for (int k = 0; k < theList.Length; k++)
+                        {
+                            if (Convert.ToInt32(theList[i]) + Convert.ToInt32(theList[j]) + Convert.ToInt32(theList[k]) == 2020)
+                            {
+                                numberOut = Convert.ToInt32(theList[i]) * Convert.ToInt32(theList[j]) * Convert.ToInt32(theList[k]);
+                                break;
+                            }
+                        }
+                    }
+                }
+                return numberOut;
+            }
+        }
+    }
+}
